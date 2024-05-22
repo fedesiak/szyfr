23d8ac1d5034059e68a8b76ab04c5f0629eb0316
fedesiak <120318227+fedesiak@users.noreply.github.com>
2024-05-22 19:18:59 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+string szyfrowanie(string a)
+{
+    string b="";
+    int r = ceil(sqrt(a.length()));
+    char tab[r][r];
+    int k = 0;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<r;j++)
+        {
+            if(k<a.length())
+            {
+                tab[i][j] = a[k];
+            }
+            else
+            {
+                tab[i][j] = '_';
+            }
+            k++;
+            cout<<tab[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<r;j++)
+        {
+            b+=tab[j][i];
+        }
+    }
+    return b;
+}
+
+int main()
+{
+    cout<<szyfrowanie("iry_nmk_faa_ot__");
+    return 0;
+}
